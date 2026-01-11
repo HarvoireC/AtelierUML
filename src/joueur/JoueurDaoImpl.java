042b381e045d59c1b2fdea8fb278a9910b677854
Corentin Harvoire <corentin.harvoire@gmail.com>
2026-01-11 18:52:11 +0100

    modification de l'ajout d'un joueur

--- src/joueur/JoueurDaoImpl.java
+++ src/joueur/JoueurDaoImpl.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JoueurDaoImpl implements JoueurDao {
-    private List<Joueur> joueurs;
+    private final List<Joueur> joueurs;
 
     public JoueurDaoImpl() {
         joueurs = new ArrayList<>();
